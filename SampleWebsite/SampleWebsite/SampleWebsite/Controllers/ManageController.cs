f034b8f66a844c574eee20db4e47f9cf26ae34e5
Kah Tang <rainmaker2k@gmail.com>
2011-07-15 09:10:23 +0200

    Samplewebsite added

---
+++ SampleWebsite/SampleWebsite/SampleWebsite/Controllers/ManageController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Text;
+
+namespace SampleWebsite.Controllers
+{
+    public class ManageController : Controller
+    {
+
+        [HandleError]
+        public virtual ActionResult Page(string pageId)
+        {
+            return new ContentResult
+            {
+                Content = String.Format("Thanks for coming to this page. I am {0}", pageId),
+                ContentType = "text/html",
+                ContentEncoding = Encoding.UTF8
+            };
+        }
+
+
+    }
+}
